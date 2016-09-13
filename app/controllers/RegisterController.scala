81e6509e3ce853a56ec5ffb17e75e40a09957b4c
小飼慎一 <kogai0121@gmail.com>
2016-09-13 21:13:52 +0900

    フォームの雛形

--- app/controllers/RegisterController.scala
+++ app/controllers/RegisterController.scala
@@ -1,16 +1,28 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import play.api.i18n._
+import play.api.data.Form
+import play.api.data.Forms._
+import models._
+
 @Singleton
-class RegisterController @Inject() extends Controller {
-  val message = "私の新しいアプリケーション"
-  def index = Action {
-    Ok(views.html.register())
+class RegisterController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  var userForm = Form {
+    mapping(
+      "email" -> text,
+      "password" -> text
+    )(UserModel.apply)(UserModel.unapply)
   }
 
-  def create = Action {
-    NotFound(views.html.register())
+  def index = Action {
+    Ok(views.html.register(userForm))
+  }
+
+  def create = Action { implicit request =>
+    val user = userForm.bindFromRequest.get
+    println(user)
+    Ok(views.html.index("ok"))
   }
 }
---
+++ app/models/UserModel.scala
@@ -0,0 +1,3 @@
+package models
+
+case class UserModel (email: String, password: String)
