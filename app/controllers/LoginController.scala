32b27f62a5d0c366c1f28f6753a8746d8cf116ab
小飼慎一 <kogai0121@gmail.com>
2016-09-24 18:12:55 +0900

    ログイン系コントローラの準備

---
+++ app/controllers/LoginController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+import play.api.i18n._
+import play.api.data.Form
+import play.api.data.Forms._
+import models._
+
+@Singleton
+class LoginController @Inject() (val messagesApi: MessagesApi) extends Controller with I18nSupport {
+  var userForm = Form {
+    mapping(
+      "email" -> email,
+      "password" -> text
+    )(UserModel.apply)(UserModel.unapply)
+  }
+
+  def index = Action {
+    Ok(views.html.login("ログインページ"))
+  }
+
+  def login = Action { implicit request =>
+    val user = userForm.bindFromRequest.get
+    if (user.email == "test@test.com" && user.password == "test") {
+      Ok(views.html.index("ログインしました"))
+    } else {
+      Forbidden(views.html.index("ログインに失敗しました"))
+    }
+  }
+
+  def logout = Action { implicit request =>
+    // セッションの削除処理
+    Ok(views.html.index("ログアウトしました"))
+  }
+}
