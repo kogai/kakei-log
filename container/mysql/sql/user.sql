d6c46f6f27fc24667f012a516a94b346a4cb5f6e
小飼慎一 <kogai0121@gmail.com>
2016-09-27 22:39:36 +0900

    ユーザtableの定義

---
+++ container/mysql/sql/user.sql
@@ -0,0 +1,24 @@
+# 0 = False 1 = True
+
+CREATE TABLE User (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT,
+  password_digest VARCHAR(255) NOT NULL,
+  mail_address VARCHAR(255) NOT NULL,
+  is_verified INTEGER(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`mail_address`)
+);
+
+DROP TABLE User;
+
+INSERT INTO User (password_digest, mail_address, is_verified) VALUES (
+  'test',
+  'test@test.com',
+  1
+), (
+  'test-password',
+  'yet-register@test.com',
+  0
+);
+
+SELECT * FROM User;
