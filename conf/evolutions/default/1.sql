2ba153efe69fef534a74aa34666aa4130b9ff0ba
小飼慎一 <kogai0121@gmail.com>
2016-10-04 09:26:26 +0900

    ユーザの初期値を挿入

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,6 +10,18 @@ CREATE TABLE User (
   UNIQUE KEY (`mail_address`)
 );
 
+INSERT INTO User (password_digest, mail_address, is_verified, verifiy_id) VALUES (
+  '$2a$10$5hodVnru4SFyYKNyAJxQU.iLnTcCRZ0N2dmwBTXGCGCseyxBPm5Q2',
+  'test@test.com',
+  1,
+  'b20eb751-2aa5-43aa-adcb-c7eed50fec36'
+), (
+  '$2a$10$1SxrNaTLjv00.Exgpm/IEeoPmT3ipVB84ZXCVme/3k7zARl.bsxcq',
+  'yet-register@test.com',
+  0,
+  'b20eb751-2aa5-43aa-adcb-c7eed50fec36'
+);
+
 # --- !Downs
 
 DROP TABLE User;
