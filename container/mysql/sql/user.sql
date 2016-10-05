e32464bb0bf8601d7d13b48e3bbc3f5c386bb02e
小飼慎一 <kogai0121@gmail.com>
2016-10-05 23:36:47 +0900

    中間テーブルを利用した検索をメモっておく

--- container/mysql/sql/user.sql
+++ container/mysql/sql/user.sql
@@ -1,25 +1,33 @@
 CREATE DATABASE kakei_log_development;
-DROP TABLE User;
-
-CREATE TABLE User (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT,
-  password_digest VARCHAR(255) NOT NULL,
-  mail_address VARCHAR(255) NOT NULL,
-  is_verified INTEGER(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (`mail_address`)
-);
-
-INSERT INTO User (password_digest, mail_address, is_verified) VALUES (
-  # 'test,
-  '$2a$10$5hodVnru4SFyYKNyAJxQU.iLnTcCRZ0N2dmwBTXGCGCseyxBPm5Q2',
-  'test@test.com',
-  1
-), (
-  # 'test-password,
-  '$2a$10$1SxrNaTLjv00.Exgpm/IEeoPmT3ipVB84ZXCVme/3k7zARl.bsxcq',
-  'yet-register@test.com',
-  0
-);
 
 SELECT * FROM User;
+
+# テーブル定義の確認
+SHOW COLUMNS FROM User;
+
+# ユーザIDが1の支払先
+SELECT *
+FROM PaymentDestination
+WHERE id IN (
+  SELECT payment_destination_id FROM UserWithPaymentDestination WHERE user_id = 1
+);
+
+# デフォルト支払先
+SELECT *
+FROM PaymentDestination
+WHERE id IN (
+  SELECT payment_destination_id FROM UserWithPaymentDestination WHERE user_id IS NULL
+);
+
+SELECT *
+FROM PaymentSource
+WHERE id IN (
+  SELECT payment_source_id FROM UserWithPaymentSource WHERE user_id IS NULL
+);
+
+# 親子カテゴリ
+SELECT *
+FROM Category
+WHERE parent_id IN (
+  SELECT id FROM Category WHERE name = "食費"
+);
